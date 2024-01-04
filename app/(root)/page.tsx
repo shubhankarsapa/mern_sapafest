ea476c82936de5e20afc3f24fecb0ff6b9836f04
Shubhankar Sapa <sapashubhankar@gmail.com>
2024-01-04 00:58:58 -0800

    implement stripe

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,34 @@
+// import CategoryFilter from "@/components/shared/CategoryFilter";
+import Collection from "@/components/shared/Collection";
+// import Search from "@/components/shared/Search";
 import { Button } from "@/components/ui/button";
+import { getAllEvents } from "@/lib/actions/event.actions";
+import { SearchParamProps } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || "";
+  const category = (searchParams?.category as string) || "";
+
+  const events = await getAllEvents({
+    query: searchText,
+    category,
+    page,
+    limit: 6,
+  });
+
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
             <h1 className="h1-bold">
-              Host, Connect, Celebrate: Your Fests, Our Platform!
+              Host, Connect, Celebrate: Your Events, Our Platform!
             </h1>
             <p className="p-regular-20 md:p-regular-24">
-              Book and learn helpful tips from 2,413+ mentors in world-class
+              Book and learn helpful tips from 3,168+ mentors in world-class
               companies with our global community.
             </p>
             <Button size="lg" asChild className="button w-full sm:w-fit">
@@ -38,9 +54,20 @@ export default function Home() {
           Trust by <br /> Thousands of Events
         </h2>
 
-        <div className="flex w-full flex-col gap-5 md:flex-row">
-          Search CategoryFilter
-        </div>
+        {/* <div className="flex w-full flex-col gap-5 md:flex-row">
+          <Search />
+          <CategoryFilter />
+        </div> */}
+
+        <Collection
+          data={events?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={page}
+          totalPages={events?.totalPages}
+        />
       </section>
     </>
   );
